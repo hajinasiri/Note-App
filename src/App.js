be547d7c1c4b5c65818afa721fb707f1572a1159
Shahab <hajinasiri@gmail.com>
2018-07-05 12:16:55 -0700

    primary routing was added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router} from 'react-router-dom';
+import Route from 'react-router-dom/Route';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <Router>
+        <div className="App">
+          <Route path='/' exact render={
+            () => {
+              return (<h1>This is the router</h1>)
+            }
+          }/>
 
-      </div>
+          <Route path='/about' exact render={
+            () => {
+              return (<h1>This is about</h1>)
+            }
+          }/>
+
+        </div>
+        </Router>
     );
   }
 }
