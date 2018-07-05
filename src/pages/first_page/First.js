ff415d21257a4ba840f26624681694f9b5efbda2
Shahab <hajinasiri@gmail.com>
2018-07-05 16:10:37 -0700

    login and register links were added to the first page

--- src/pages/first_page/First.js
+++ src/pages/first_page/First.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
 
 class First extends Component {
   render() {
     return (
-      <h1> First page hooora! </h1>
+      <div>
+        <h1> First page hooora! </h1>
+        <div>
+          <Link to='/login'>Log in </Link>
+        </div>
+        <div>
+          <Link to='/register'>Register </Link>
+        </div>
+
+      </div>
     )
   }
 }
