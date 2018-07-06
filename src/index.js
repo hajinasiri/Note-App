cf9f92813a8e05f392e6d969a91bccb71bb08992
Shahab <hajinasiri@gmail.com>
2018-07-06 14:24:24 -0700

    apollo client was added

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloProvider} from 'react-apollo';
+import { ApolloClient } from 'apollo-client'
+import { HttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
 
 import Routes from './routes';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
+
+const client = new ApolloClient({
+  link: new HttpLink( 'localhost:3000/graphql' ),
+  cache: new InMemoryCache()
+})
+
 const App = () => (
-  <Routes />
+  <ApolloProvider client = {client}>
+    <Routes />
+  </ApolloProvider>
 );
 
 ReactDOM.render(<App />, document.getElementById('root'));
