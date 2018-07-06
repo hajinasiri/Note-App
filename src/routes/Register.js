f65e21da6c2c9772373fd593b5690c1e849821a5
Shahab <hajinasiri@gmail.com>
2018-07-06 16:23:25 -0700

    username, email and password fields were set

--- src/routes/Register.js
+++ src/routes/Register.js
@@ -1,9 +1,30 @@
 import React from 'react';
 
 export default class Register extends React.Component {
+  state = {
+    username:'',
+    email:'',
+    password:'',
+    isAdmin:false
+  }
+
+  onChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  onSubmit = () => {
+
+  }
   render() {
     return (
-      <h1>Register page</h1>
+      <div>
+        <input name='username' placeholder='Username' onChange={e => this.onChange(e)} value={this.state.username}/>
+        <input name='email' placeholder='Email' onChange={e => this.onChange(e)} value={this.state.email} />
+        <input name='password' placeholder='Password' type='password' onChange={e => this.onChange(e)} value={this.state.password} />
+        <button onClick={() => this.onSubmit()} type='primary' > Primary </button>
+      </div>
     )
   }
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -10,8 +10,8 @@ export default () => (
   <Router>
     <Switch>
       <Route exact path="/login"    render = {props => <Login {...props}/>}/>
-      <Route exact path="/auth"     render = {props => <Login {...props}/>}/>
-      <Route exact path="/register" render = {props => <Auth {...props}/>}/>
+      <Route exact path="/auth"     render = {props => <Auth{...props}/>}/>
+      <Route exact path="/register" render = {props => <Register {...props}/>}/>
     </Switch>
   </Router>
 
