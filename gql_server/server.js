b6a4982dc9848197810223517e8e260cccb7967f
Shahab <hajinasiri@gmail.com>
2018-07-14 10:53:33 -0700

    token creating and verification code was added. User gets extracted from the user and passed to the context

--- gql_server/server.js
+++ gql_server/server.js
@@ -2,18 +2,33 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import { graphiqlExpress, graphqlExpress } from 'graphql-server-express';
 import { makeExecutableSchema } from 'graphql-tools';
-import SEC from '../secret.json';
+import jwt from 'jsonwebtoken';
+import cors from 'cors';
 
 import typeDefs from './schema';
 import resolvers from './resolvers';
 import models from './models';
+import SEC from '../secret.json';
 
-import cors from 'cors';
 
 const schema = makeExecutableSchema({
   typeDefs,
   resolvers,
 });
+
+//To get the token from header and verify it
+//The response {user} will be the data we passed in resolver meaninig user: _.pick(user, ['id', 'username'])
+const addUser = async (req) => {
+  const token = req.headers.authorization;
+  try {
+    const { user } = await jwt.verify(token, SECRET);
+    req.user = user;
+  } catch (err) {
+    console.log(err);
+  }
+  req.next();
+};
+app.use(addUser);
 
 const SECRET = SEC.SECRET;
 
@@ -32,7 +47,13 @@ app.use(
 app.use(
   '/graphql',
   bodyParser.json(),
-  graphqlExpress({ schema, context:{models,SECRET}}),
+  graphqlExpress(req => ({
+    schema,
+    context: {
+      models,
+      SECRET,
+      user: req.user,
+    },
+  })),
 );
-
 models.sequelize.sync().then(() => app.listen(4000));
