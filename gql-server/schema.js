fbb6247b2af2af47028aecd04b5016668f6015dd
Shahab <hajinasiri@gmail.com>
2018-07-16 17:35:06 -0700

    restoring the deleted code

---
+++ gql-server/schema.js
@@ -0,0 +1,19 @@
+export default `
+  type User {
+    id: Int!
+    username: String!
+    createdAt: String!
+    updatedAt: String!
+    email: String!
+  }
+  type Query {
+    allUsers: [User!]!
+    me: User
+  }
+  type Mutation {
+    register(username: String!, email: String!, password: String!): User!
+    updateUser(username: String!, newUsername: String!): [Int!]!
+    deleteUser(username: String!): Int!
+    login(email: String!, password: String!): String!
+  }
+`;
